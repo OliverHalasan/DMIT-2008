fab466de335b325bc2d9eac93fe6d773b95841a6
Oliver Halasan <john_oliver30@yahoo.com>
2022-12-01 22:09:39 -0700

    with of button

--- components/navigation/DeploymenLink.jsx
+++ components/navigation/DeploymenLink.jsx
@@ -9,7 +9,7 @@
 import Link from 'next/link'
 function GithubButton({className, ...props}) {
     return (
-         <Link href="https://dmit-2008.vercel.app/" className={`hidden ${className} w-[130px]`}>
+         <Link href="https://dmit-2008.vercel.app/" className={`hidden ${className}`}>
          <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Deploy</button>
      </Link>
      );
